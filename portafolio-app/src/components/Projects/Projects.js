34417b085260ddb94595f94b42b0e24ef4ef20b0
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2021-02-08 22:35:11 -0600

    Estilos componente Projects

--- portafolio-app/src/components/Projects/Projects.js
+++ portafolio-app/src/components/Projects/Projects.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 export const Projects = () => {
+
+    const handleProjectClick = (type) => {
+        console.log({type});
+    };
+
     return (
         <div className="projects__container">
-            
+            <div className="projects__title">
+                <h3>Projects(3)</h3>
+            </div>
+            <div className="projects__types">
+                <div 
+                    className="projects__responsive"
+                    onClick={ () => handleProjectClick('R') }
+                >
+                    <p>React: Responsive</p>
+                </div>
+                <div 
+                    className="projects__api"
+                    onClick={ () => handleProjectClick('A') }
+                >
+                    <p>React: Use of API</p>
+                </div>
+                <div 
+                    className="projects__mern"
+                    onClick={ () => handleProjectClick('M') }
+                >
+                    <p>MERN</p>
+                </div>
+            </div>
         </div>
     );
 };
--- portafolio-app/src/components/Skills/Skill.js
+++ portafolio-app/src/components/Skills/Skill.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 export const Skill = (props) => {
 
-    console.log(props);
-
     return (
+
         <div className="skills__techs-range">
             <div className="skills__list">
                 <div className="skills__tech">
@@ -16,5 +15,7 @@ export const Skill = (props) => {
                 <div className="skills__range__relleno" style={{ width: `${props.tech.porcentage}%` }}></div>
             </div>
         </div>
+
     );
+    
 };
