90cec23e27906f57140fb6501730ea3665abafee
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2021-02-14 21:42:46 -0600

    Fotografía

--- portafolio-app/src/components/PersonalInfo/PersonalInfo.js
+++ portafolio-app/src/components/PersonalInfo/PersonalInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Email from '@material-ui/icons/Email';
 import Phone from '@material-ui/icons/Phone';
-import cosmos from './../../assets/cosmos.png';
+import yo from './../../assets/yo.PNG';
 
 export const PersonalInfo = () => {
 
@@ -9,7 +9,7 @@ export const PersonalInfo = () => {
 
         <div className="personalInfo__container">
             <div className="personalInfo__foto">
-                <img src={ cosmos } alt="yo"/>
+                <img src={ yo } alt="yo"/>
             </div>
             <div className="personalInfo__personal">
                 <div className="personalInfo__name-email-phone">
